e126eabef638f207428aad4acfbc481f818ee924
Taisur Rubayet <125201336+Taisur-Rubayet@users.noreply.github.com>
2026-09-01 22:01:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -39,54 +39,88 @@ cropBtn.addEventListener('click', () => {
   const croppedCanvas = cropper.getCroppedCanvas({ width:1080, height:1080 });
 
   function finalize() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle="#ffffff";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage(croppedCanvas,0,0,canvas.width,canvas.height);
-    ctx.drawImage(frame,0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Update preview
-    preview.src = canvas.toDataURL("image/png");
+  // White background
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Hide crop button after generation
-    cropBtn.style.display = 'none';
-    downloadBtn.style.display = 'inline-block';
+  // Draw cropped photo
+  ctx.drawImage(
+    croppedCanvas,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
 
-    // Show caption
-    captionTextDiv.innerHTML = `
-  DIU Textile Alumni 4th Mega Reunion 2026<br>
-  Frame link: <a href="https://taisur-rubayet.github.io/4th-textile-alumni-reunion-Dp-frame/" target="_blank">https://taisur-rubayet.github.io/4th-textile-alumni-reunion-Dp-frame/<br><br>
-  #DIUTextileAlumniMegaReunion2026<br>
-  #DIUTextileFamily<br>
-  #MegaReunion2026<br><br>
-  </a>
-`;
-    captionContainer.style.display = 'block';
+  // Draw frame while removing black background
+  const frameCanvas = document.createElement("canvas");
+  frameCanvas.width = canvas.width;
+  frameCanvas.height = canvas.height;
 
-    if(cropper){ cropper.destroy(); cropper=null; }
+  const frameCtx = frameCanvas.getContext("2d");
+  frameCtx.drawImage(
+    frame,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  const imageData = frameCtx.getImageData(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  const data = imageData.data;
+
+  // Make black background transparent
+  for (let i = 0; i < data.length; i += 4) {
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+
+    if (r < 25 && g < 25 && b < 25) {
+      data[i + 3] = 0;
+    }
   }
 
-  if(frame.complete){
-    finalize();
-  } else {
-    frame.onload = finalize;
+  frameCtx.putImageData(imageData, 0, 0);
+
+  // Draw transparent frame over photo
+  ctx.drawImage(
+    frameCanvas,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  // Update preview
+  preview.src = canvas.toDataURL("image/png");
+
+  cropBtn.style.display = "none";
+  downloadBtn.style.display = "inline-block";
+
+  // Show caption
+  captionTextDiv.innerHTML = `
+    DIU Textile Alumni 4th Mega Reunion 2026<br><br>
+    #DIUTextileAlumniMegaReunion2026<br>
+    #DIUTextileFamily<br>
+    #MegaReunion2026<br><br>
+    Frame link:
+    <a href="https://taisur-rubayet.github.io/4th-textile-alumni-reunion-Dp-frame/" target="_blank">
+      https://taisur-rubayet.github.io/4th-textile-alumni-reunion-Dp-frame/
+    </a>
+  `;
+
+  captionContainer.style.display = "block";
+
+  if (cropper) {
+    cropper.destroy();
+    cropper = null;
   }
-});
-
-// Copy caption
-copyCaptionBtn.addEventListener('click', () => {
-  const textToCopy = `DIU Textile Alumni 4th Mega Reunion 2026
-Frame link: https://taisur-rubayet.github.io/4th-textile-alumni-reunion-Dp-frame/
-
-#DIUTextileAlumniMegaReunion2026
-#DIUTextileFamily
-#MegaReunion2026`;
-});
-
-// Download DP
-downloadBtn.addEventListener('click', () => {
-  const link = document.createElement('a');
-  link.download = 'Reunion_DP.png';
-  link.href = canvas.toDataURL("image/png");
-  link.click();
-});
+}
